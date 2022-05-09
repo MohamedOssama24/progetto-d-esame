075943fbaf38d3f627a2978afa859b5d8b535a71
MohamedOssama24 <81478211+MohamedOssama24@users.noreply.github.com>
2022-05-09 09:52:35 +0200

    Initial version of myapp generated by generator-jhipster@7.8.1

---
+++ myApp/src/main/java/org/jhipster/myapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.jhipster.myapp.service.dto;
---
+++ myApp/src/test/java/org/jhipster/myapp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package org.jhipster.myapp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.jhipster.myapp.MyappApp;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = MyappApp.class)
+public @interface IntegrationTest {
+}
